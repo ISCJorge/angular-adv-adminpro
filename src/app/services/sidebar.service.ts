38bdc990b2d2a730642e5a5da528914a453841c2
Jorge <iscjorgemonroy@gmail.com>
2020-12-20 23:12:37 -0600

    Fin Sección 8

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -12,7 +12,9 @@ export class SidebarService {
       submenu: [
         { titulo: 'Main', url: '/' },
         { titulo: 'Progress Bar', url: 'progress' },
-        { titulo: 'Graficas', url: 'grafica1' }
+        { titulo: 'Graficas', url: 'grafica1' },
+        { titulo: 'Promesas', url: 'promesas' },
+        { titulo: 'RXJS', url: 'rxjs' }
       ]
     }
   ];
